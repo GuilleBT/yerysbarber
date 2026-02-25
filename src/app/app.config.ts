61cc8a8faa0bf319db8819c4d326d62cd91cfd11
GuilleBT <guillermobeltrantabares@gmail.com>
2026-02-25 19:00:39 +0100

    Conexion con firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,26 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAyDK9dnzkKXkOASvSkh4HGHwqGH4-lvk4",
+  authDomain: "yerysbarber.firebaseapp.com",
+  projectId: "yerysbarber",
+  storageBucket: "yerysbarber.firebasestorage.app",
+  messagingSenderId: "643914939712",
+  appId: "1:643914939712:web:7287a7703e3857b8448ea5"
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
   ]
 };
